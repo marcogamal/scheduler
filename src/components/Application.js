f85448655249f0649337cc24ffe830f16a4b47c2
Marco <marco.salman@gmail.com>
2022-05-22 09:11:55 -0400

    Addint State to our App

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
-
 
 const days = [
   {
@@ -21,30 +20,25 @@ const days = [
   },
 ];
 
-
-
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
         <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-  <DayList
-  days={days}
-  day={"Monday"}
-  setDay={day => console.log(day)}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={days} day={day} setDay={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
