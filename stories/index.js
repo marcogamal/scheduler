7826a5dec0af6aff3650d95efd642c840d58c37f
Marco <marco.salman@gmail.com>
2022-05-23 16:05:25 -0400

    create appointment component

--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,7 @@ import Button from "components/Button";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -125,3 +126,10 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+  storiesOf("Appointment", module)
+  .addParameters({
+      backgrounds: [{ name: "white", value: "#fff", default: true }]
+    })
+    .add("Appointment", () => <Appointment />)
+    .add("Appointment with Time",  () => <Appointment time="12pm" />);
