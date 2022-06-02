3b4ab95aa33a946787ee893b42b0a79b6421b7ff
Marco <marco.salman@gmail.com>
2022-06-01 21:02:44 -0400

    building a hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); 
+  function transition (newMode, replace = false) {
+    if (!replace) {
+      setHistory(prev => [...prev, newMode])
+      setMode(newMode)
+    }
+    setMode(newMode)
+  }
+
+  // Set the previous mode then remove the last one from history
+  function back () {
+    if (history.length > 1) {
+      const newHistory = history.slice(0, -1)
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
+} 
