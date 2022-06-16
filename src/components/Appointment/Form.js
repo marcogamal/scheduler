180e43faca38aadee559fc22fef3f84cf7f56c16
Marco <marco.salman@gmail.com>
2022-06-16 19:36:10 -0400

    making first test

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
+const [error, setError] = useState("");
 
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const reset = () => {
-    setStudent("")
-    setInterviewer(null)
-  }
+    setStudent("");
+    setInterviewer(null);
+  };
   const cancel = () => {
     reset();
     props.onCancel();
+  };
+  function onSave() {
+    !student
+      ? setError("Student name cannot be blank")
+      : !interviewer
+      ? setError("Interviewer must be choose")
+      : props.onSave(student, interviewer);
   }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -24,9 +32,10 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => {
-              setStudent(event.target.value)
+              setStudent(event.target.value);
             }}
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={props.interviewers}
---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+import Appointment from "components/Appointment";
+import Form from "components/Appointment/Form";
+import Application from "components/Application";
+
+afterEach(cleanup);
+
+it("renders without crashing", () => {
+  render(<Application />);
+});
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+});
+
+describe("Form", () => {
+  const interviewers = [
+    {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png",
+    },
+  ];
+
+  it("renders without student name if not provided", () => {
+    const { getByPlaceholderText } = render(
+      <Form interviewers={interviewers} />
+    );
+    expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+  });
+
+  it("renders with initial student name", () => {
+    const { getByTestId } = render(
+      <Form interviewers={interviewers} student="Lydia Miller-Jones" />
+    );
+    expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
+  });
+});
